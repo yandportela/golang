0c51d7d72ed8872d9e2f951f0d98a82369ebf085
yanportela <yanportela2009@outlook.com>
2025-03-28 08:19:58 -0300

    operador lógico

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import "fmt"
 
 func main() {
 	a, b := 10, 3
-	a++
 	fmt.Println("A soma é: ", a + b)
 	fmt.Println("A subtração é: ", a - b)
 	fmt.Println("A multiplicação é: ", a * b)
 	fmt.Println("A divisão é: ", a / b)
 	fmt.Println("O resto da divisão é: ", a % b)
+	a++
+	fmt.Println("O incremento de A é: ", a)
+	if a > 0 && b > 0 {
+		fmt.Println("A e B são maiores que zero")
+	}
 }
