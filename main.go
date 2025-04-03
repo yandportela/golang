26d4b95976f26cde4ec49b449e42dbbe522877f3
yanportela <yanportela2009@outlook.com>
2025-04-03 09:18:57 -0300

    vetor.3

--- main.go
+++ main.go
@@ -9,4 +9,20 @@ func main() {
 	fmt.Println(nomes)
 	nomes[3] = "Morales"
 	fmt.Println(nomes)
+	// Slice
+	var score = [4]int{100, 200, 300, 400}
+	fmt.Println(score)
+	score[1] = 2
+	fmt.Println(score, len(score), cap(score))
+	rangeOne := score[1:3]
+	fmt.Println(rangeOne)
+	rangeTwo := score[2:]
+	fmt.Println(rangeTwo)
+	rangeThree := score[:3]
+	fmt.Println(rangeThree)
+
+	var superherois = []string{"Deadpool", "Homem-Aranha", "Motoqueiro Fantasma"}
+	fmt.Println(superherois)
+	superherois = append(superherois, "Ben 10", "Ciborgue")
+	fmt.Println(superherois, len(superherois), cap(superherois))
 }
