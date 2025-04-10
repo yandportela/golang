1c9405017cc2c79912d963adda9d09f8d3406dca
yanportela <yanportela2009@outlook.com>
2025-04-10 10:59:36 -0300

    exercicio 2

--- main.go
+++ main.go
@@ -5,24 +5,73 @@ import (
 )
 
 func main() {
-	var idade int
-	fmt.Println("Por favor digite sua idade:")
-	fmt.Scan(&idade)
-	if idade < 0 || idade > 120 {
+	var saldo float64
+	var continuar string
+	var opcao string
+	fmt.Print("Digite seu saldo inicial: ")
+	fmt.Scan(&saldo)
 	for {
-		fmt.Println("Idade inválida.")
-		fmt.Println("Por favor digite uma idade válida:")
-		fmt.Scan(&idade)
-		if (idade > 0 && idade < 120) {
+		if saldo < 0 {
+			fmt.Println("Saldo negativo. Por favor, insira um saldo válido.")
+			fmt.Print("Digite seu saldo inicial: ")
+			fmt.Scan(&saldo)
+		} else {
 			break
 		}
 	}
-	}
-	if idade < 18 {
-		fmt.Println("Você é menor de idade.")
-	}	else if idade >= 18 && idade < 60 {
-		fmt.Println("Você é maior de idade.")
-	} else {
-		fmt.Println("Você é idoso.")
+
+	for {
+		for {
+			fmt.Print("Você deseja sacar ou depositar? (s/d): ")
+			fmt.Scan(&opcao)
+			if opcao == "s" || opcao == "d" {
+				break
+			} else {
+				fmt.Println("Opção inválida. Por favor, escolha 's' para sacar ou 'd' para depositar.")
+			}
+		}
+
+		if opcao == "s" {
+			var valorSaque float64
+			fmt.Print("Digite o valor do saque: ")
+			fmt.Scan(&valorSaque)
+			if valorSaque > saldo {
+				fmt.Println("Saldo insuficiente.")
+			} else {
+				saldo -= valorSaque
+				fmt.Printf("Saque de %.2f realizado com sucesso. Saldo atual: %.2f\n", valorSaque, saldo)
+			}
+			fmt.Print("Você deseja realizar outra operação? (s/n): ")
+			fmt.Scan(&continuar)
+			if continuar == "s" {
+				main()
+			} else {
+				fmt.Println("Obrigado por usar nosso sistema!")
+				break
+			}
+		}
+
+		if opcao == "d" {
+			var valorDeposito float64
+			for {
+				fmt.Print("Digite o valor do depósito: ")
+				fmt.Scan(&valorDeposito)
+				if valorDeposito <= 0 {
+					fmt.Println("Valor inválido. O depósito deve ser maior que zero.")
+				} else {
+					saldo += valorDeposito
+					fmt.Printf("Depósito de %.2f realizado com sucesso. \nSaldo atual: %.2f\n", valorDeposito, saldo)
+					break
+				}
+			}
+
+			fmt.Print("Você deseja realizar outra operação? (s/n): ")
+			fmt.Scan(&continuar)
+			if continuar == "s" {
+			} else {
+				fmt.Println("Obrigado por usar nosso sistema!")
+				break
+			}
+		}
 	}
 }
