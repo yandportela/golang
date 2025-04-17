f0eb64521bde4706b20dc1db5db9423c7022899b
yanportela <yanportela2009@outlook.com>
2025-04-17 10:59:58 -0300

    map

--- main.go
+++ main.go
@@ -2,26 +2,21 @@ package main
 
 import ("fmt")
 
-var nome string
-var idade int
-
-func dadosPessoa(nome string, idade int) (int, string) {
-	if idade <= 0 || idade > 120 {
-		return idade, "\n"+nome+" idade inválida"
-	} else if idade <= 17 {
-		return idade, "\n"+nome+" é menor de idade"
-	} else if idade <= 59 {
-		return idade, "\n"+nome + " é maior de idade"
-	} else {
-		return idade, "\n"+nome + " é idoso"
-	}
-}
-
 func main() {
-	fmt.Println("Por favor digite seu nome:")
-	fmt.Scan(&nome)
-	fmt.Println("Por favor digite sua idade:")
-	fmt.Scan(&idade)
-	fmt.Print("\n\n")
-	fmt.Println(dadosPessoa(nome, idade))
+	alunoIdade := make(map[string]int)
+	alunoIdade["Bruno"] = 15
+	alunoIdade["Otávio"] = 16
+	alunoIdade["Fabiano"] = 40
+	alunoIdade["Isabela"] = 15
+	fmt.Println("Idade do Bruno", alunoIdade["Bruno"])
+
+	notasAlunos := map[string]float64{
+		"Bruno" : 9.7,
+		"Otávio" : 10,
+		"Fabiano" : 8.7,
+		"Isabela" : 9.5,
+	}
+	for nome,nota := range notasAlunos{
+		fmt.Printf("%s tirou %f \n", nome, nota)
+	}
 }
