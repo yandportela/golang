d7a27a47a7bc58d31397102fcc918b51c5926b43
yanportela <yanportela2009@outlook.com>
2025-04-17 08:28:14 -0300

    funcs.2

--- main.go
+++ main.go
@@ -2,58 +2,30 @@ package main
 
 import ("fmt")
 
-var saldo, valorSaque, valorDeposito int
-var opcao int
-
-func sacar() {
-	if saldo == 0 {
-		fmt.Println("\nVocê não tem saldo suficiente.")
-	} else {
-	fmt.Println("\nQuanto de", saldo, "real(is) você quer sacar?")
-	fmt.Scanln(&valorSaque)
-	if valorSaque > saldo || valorSaque < 0 {
-		fmt.Println("\nVocê não tem saldo suficiente.")
-	} else {
-		saldo -= valorSaque
-		fmt.Println("\nVocê sacou", valorSaque, "real(is) e agora tem", saldo, "real(is).")
+func dividir (dividendo, divisor int)(int, string) {
+	if divisor == 0 {
+		return 0, "Erro na divisão por zero"
 	}
-}
-}
-
-func depositar() {
-	fmt.Println("Quanto você quer depositar?")
-	for {
-		fmt.Scanln(&valorDeposito)
-		if valorSaque < 0 {
-			fmt.Println("Valor inválido. Tente novamente.")
-			continue
-		}
-		break
-	}
-	saldo += valorDeposito
-	fmt.Println("\nVocê depositou", valorDeposito, "real(is) e agora tem", saldo, "real(is).")
+	return dividendo / divisor, "Sem erro"
 }
 
-func mostrarSaldo() {
-	fmt.Println("\nSeu saldo é:", saldo, "real(is).")
+func operacaoBasica(a int, b int) (int, int, int) {
+	soma := a+b
+	multiplicacao := a*b
+	subtracao := a-b
+	return soma, multiplicacao, subtracao
 }
+
 func main() {
-	fmt.Println("Bem-vindo ao banco!")
-	for {
-		fmt.Println("\n\n\nEscolha uma opção: \n1. Sacar \n2. Depositar \n3. Mostrar saldo\n4. Sair")
-		fmt.Scanln(&opcao)
-		switch opcao {
-		case 1:
-			sacar()
-		case 2:
-			depositar()
-		case 3:
-			mostrarSaldo()
-		case 4:
-			fmt.Println("Saindo...")
-			return
-		default:
-			fmt.Println("Opção inválida.")
-		}
+	resultado, erro := dividir(10,2)
+	if erro != "Sem erro" {
+		fmt.Println("Erro")
+	} else {
+		fmt.Println("O resultado da divisão é:", resultado, erro)
 	}
+
+	soma, multi, sub := operacaoBasica(10,2)
+	fmt.Println(soma)
+	fmt.Println(multi)
+	fmt.Println(sub)
 }
