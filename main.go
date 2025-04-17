37cdde352777e0323a1a2c5deb06409d30f41093
yanportela <yanportela2009@outlook.com>
2025-04-17 10:38:14 -0300

    funcao informacoes

--- main.go
+++ main.go
@@ -2,30 +2,26 @@ package main
 
 import ("fmt")
 
-func dividir (dividendo, divisor int)(int, string) {
-	if divisor == 0 {
-		return 0, "Erro na divisão por zero"
-	}
-	return dividendo / divisor, "Sem erro"
-}
+var nome string
+var idade int
 
-func operacaoBasica(a int, b int) (int, int, int) {
-	soma := a+b
-	multiplicacao := a*b
-	subtracao := a-b
-	return soma, multiplicacao, subtracao
+func dadosPessoa(nome string, idade int) (int, string) {
+	if idade <= 0 || idade > 120 {
+		return idade, "\n"+nome+" idade inválida"
+	} else if idade <= 17 {
+		return idade, "\n"+nome+" é menor de idade"
+	} else if idade <= 59 {
+		return idade, "\n"+nome + " é maior de idade"
+	} else {
+		return idade, "\n"+nome + " é idoso"
+	}
 }
 
 func main() {
-	resultado, erro := dividir(10,2)
-	if erro != "Sem erro" {
-		fmt.Println("Erro")
-	} else {
-		fmt.Println("O resultado da divisão é:", resultado, erro)
-	}
-
-	soma, multi, sub := operacaoBasica(10,2)
-	fmt.Println(soma)
-	fmt.Println(multi)
-	fmt.Println(sub)
+	fmt.Println("Por favor digite seu nome:")
+	fmt.Scan(&nome)
+	fmt.Println("Por favor digite sua idade:")
+	fmt.Scan(&idade)
+	fmt.Print("\n\n")
+	fmt.Println(dadosPessoa(nome, idade))
 }
