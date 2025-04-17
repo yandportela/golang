d174366af778a46dd55f6a48ab43fdfaa5800e19
yanportela <yanportela2009@outlook.com>
2025-04-17 08:05:18 -0300

    Exemplo função banco

--- main.go
+++ main.go
@@ -1,18 +1,59 @@
 package main
 
-import (
-	"fmt"
-)
+import ("fmt")
 
-func sayHello(nome string) {
-	fmt.Println("Olá", nome)
+var saldo, valorSaque, valorDeposito int
+var opcao int
+
+func sacar() {
+	if saldo == 0 {
+		fmt.Println("\nVocê não tem saldo suficiente.")
+	} else {
+	fmt.Println("\nQuanto de", saldo, "real(is) você quer sacar?")
+	fmt.Scanln(&valorSaque)
+	if valorSaque > saldo || valorSaque < 0 {
+		fmt.Println("\nVocê não tem saldo suficiente.")
+	} else {
+		saldo -= valorSaque
+		fmt.Println("\nVocê sacou", valorSaque, "real(is) e agora tem", saldo, "real(is).")
+	}
+}
 }
 
-func addNumbers(a, b int) int {
-	return a + b
+func depositar() {
+	fmt.Println("Quanto você quer depositar?")
+	for {
+		fmt.Scanln(&valorDeposito)
+		if valorSaque < 0 {
+			fmt.Println("Valor inválido. Tente novamente.")
+			continue
+		}
+		break
+	}
+	saldo += valorDeposito
+	fmt.Println("\nVocê depositou", valorDeposito, "real(is) e agora tem", saldo, "real(is).")
+}
+
+func mostrarSaldo() {
+	fmt.Println("\nSeu saldo é:", saldo, "real(is).")
 }
 func main() {
-	sayHello("Yan")
-	resultado := addNumbers(1, 2)
-	fmt.Println("Resultado: ", resultado)
+	fmt.Println("Bem-vindo ao banco!")
+	for {
+		fmt.Println("\n\n\nEscolha uma opção: \n1. Sacar \n2. Depositar \n3. Mostrar saldo\n4. Sair")
+		fmt.Scanln(&opcao)
+		switch opcao {
+		case 1:
+			sacar()
+		case 2:
+			depositar()
+		case 3:
+			mostrarSaldo()
+		case 4:
+			fmt.Println("Saindo...")
+			return
+		default:
+			fmt.Println("Opção inválida.")
+		}
+	}
 }
